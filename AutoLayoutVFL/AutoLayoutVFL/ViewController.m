1eac89dafd54be582c596d3e7818fcaa35b987be
susu <28025920@qq.com>
2016-04-19 20:22:32 +0800

    Using VFL to implement AutoLayout.

--- AutoLayoutVFL/AutoLayoutVFL/ViewController.m
+++ AutoLayoutVFL/AutoLayoutVFL/ViewController.m
@@ -16,12 +16,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UIView* blueView=[[UIView alloc]init];
+    blueView.backgroundColor=[UIColor blueColor];
+    //取消Autoresizing
+    blueView.translatesAutoresizingMaskIntoConstraints=NO;
+    [self.view addSubview:blueView];
+    
+    UIView* redView=[[UIView alloc]init];
+    redView.backgroundColor=[UIColor redColor];
+    redView.translatesAutoresizingMaskIntoConstraints=NO;
+    [self.view addSubview:redView];
+    
+    //通过VFL设置约束
+    NSNumber* margin=@20;
+    NSDictionary* metrics=NSDictionaryOfVariableBindings(margin);
+    NSDictionary* views=NSDictionaryOfVariableBindings(blueView,redView);
+    NSString* VFL=@"H:|-margin-[blueView]-margin-[redView(==blueView)]-margin-|";
+    NSArray* Hcon=[NSLayoutConstraint constraintsWithVisualFormat:VFL options: NSLayoutFormatAlignAllTop|NSLayoutFormatAlignAllBottom metrics:metrics views:views];
+    [self.view addConstraints:Hcon];
+    
+    NSString* VFL2=@"V:|-margin-[blueView(40)]";
+    NSArray* Vcon=[NSLayoutConstraint constraintsWithVisualFormat:VFL2 options:kNilOptions metrics:metrics views:views];
+    [self.view addConstraints:Vcon];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
